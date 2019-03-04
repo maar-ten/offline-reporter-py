5a08f2c3e71bf8eb7e0423c87f3829fbc337f122
maar-ten <maar-ten@users.noreply.github.com>
2019-03-04 19:22:19 +0100

    change code style to Python standard

--- offline-reporter.py
+++ offline-reporter.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
 
-import logging
 import time
-import socket
 
+import logging
+import socket
 from datetime import datetime
 
 CONNECTION_TIMEOUT = 5
 THREAD_SLEEP = 10
 
+
 def main():
-    startLogger()
+    start_logger()
 
     while True:
-        if True == isOffline():
-            logConnectionError()
-        
+        if is_offline():
+            log_connection_error()
+
         time.sleep(THREAD_SLEEP)
 
-def startLogger():
+
+def start_logger():
     today = datetime.now().date().isoformat()
-    datedFilename = 'errors_{when}.log'.format(when = today)
+    dated_filename = 'errors_{when}.log'.format(when=today)
 
     logging.basicConfig(
-        filename = datedFilename, 
-        format = '%(asctime)s %(message)s', 
-        level = logging.ERROR
+        filename=dated_filename,
+        format='%(asctime)s %(message)s',
+        level=logging.ERROR
     )
 
-def isOffline():
+
+def is_offline():
     try:
         socket.setdefaulttimeout(CONNECTION_TIMEOUT)
-        testSocket = socket.socket().connect(('8.8.8.8', 53))
-        testSocket.close()
+        socket.socket().connect(('8.8.8.8', 53))
         return False
     except Exception:
         return True
 
-def logConnectionError():
+
+def log_connection_error():
     logging.error('cannot reach the internet')
+
 
 if __name__ == '__main__':
     main()
