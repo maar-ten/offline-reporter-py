743d3016fe1d89daf289de3e0c029ffc0a992042
maar-ten <maar-ten@users.noreply.github.com>
2019-03-05 19:24:00 +0100

    Replaced loop with observable interval and calculated offline time

--- offline-reporter.py
+++ offline-reporter.py
@@ -2,46 +2,38 @@
 
 import logging
 import socket
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from rx import Observable
 
-CONNECTION_TIMEOUT = 5
-THREAD_SLEEP = 10
-INTERVAL_TIME = 10000
+CONNECTION_TIMEOUT_SEC = 5
+INTERVAL_TIME_MS = 2000
 
 
 def main():
     start_logger()
 
-    numbers = [1, 2, 3, 4, 5]
-    all = Observable.from_(numbers)
-    all.subscribe(lambda number: print('The number is {}'.format(number)))
+    connectivity = Observable.interval(INTERVAL_TIME_MS) \
+        .map(lambda i: is_online())
 
-    Observable.interval(INTERVAL_TIME).time_interval().take(5).subscribe(lambda i: print('we are here!'))
+    is_offline = connectivity.filter(lambda online: online is False)
+    is_offline.subscribe(lambda i: logging.error('cannot reach the internet'))
 
-    # connectivity = Observable.interval(INTERVAL_TIME) \
-    #     .flat_map(lambda i: is_online())
-    #
-    # is_offline = connectivity.filter(lambda online: online is False)
-    # is_offline.subscribe(lambda i: logging.error('cannot reach the internet'))
-    #
-    # how_long_was_the_internet_down = connectivity \
-    #     .distinct_until_changed(lambda online: online is False) \
-    #     .skip(1) \
-    #     .time_interval() \
-    #     .filter(lambda connectivity_changed: connectivity_changed.value is True) \
-    #     .map(lambda connectivity_changed: connectivity_changed.interval)
-    #
-    # how_long_was_the_internet_down.subscribe(lambda interval: print(interval))
+    how_long_was_the_internet_down = connectivity \
+        .distinct_until_changed(lambda online: online is False) \
+        .skip(1) \
+        .time_interval() \
+        .filter(lambda connectivity_changed: connectivity_changed.value is True) \
+        .map(lambda connectivity_changed: connectivity_changed.interval) \
+        .map(lambda interval: interval + timedelta(milliseconds=INTERVAL_TIME_MS)) \
+        .map(lambda timedelta: timedelta.total_seconds()) \
+        .map(lambda seconds: int(seconds)) \
+        .map(lambda seconds: '{sec} seconds'.format(sec=seconds)) \
+        .map(lambda seconds_str: 'Internet was down for {sec_str}'.format(sec_str=seconds_str))
 
-    # .filter(connectivityChanged => connectivityChanged.value === true)
-    # .map(connectivityChanged => connectivityChanged.interval)
-    # .map(interval => moment.duration(interval))
-    # .map(duration => duration.add(connectivityTimeout, 'ms'))
-    # .map(duration => duration.as('seconds'))
-    # .map(duration => Math.round(duration) + ' seconds')
-    # .map(txtDuration => 'Internet was down for ' + txtDuration + ' :unamused:');
+    how_long_was_the_internet_down.subscribe(lambda interval: print(interval))
+
+    input('Program is running. Press enter to exit\n')
 
 
 def start_logger():
@@ -57,7 +49,7 @@ def start_logger():
 
 def is_online():
     try:
-        socket.setdefaulttimeout(CONNECTION_TIMEOUT)
+        socket.setdefaulttimeout(CONNECTION_TIMEOUT_SEC)
         socket.socket().connect(('8.8.8.8', 53))
         return True
     except Exception:
